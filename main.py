e2547f9bdb063782da6a2ff6837d940025184493
Yang Jae Hoon <didwogns789@yonsei.ac.kr>
2021-03-24 14:25:52 +0900

    Set up project

---
+++ main.py
@@ -0,0 +1,13 @@
+from ui.ui import Ui_class
+
+
+class Main:
+    def __init__(self):
+        print('실행할 Main 클래스 입니다.')
+
+        Ui_class()
+
+
+if __name__ == '__main__':
+    Main()
+
---
+++ ui/ui.py
@@ -0,0 +1,8 @@
+from kiwoom.kiwoom import Kiwoom
+
+
+class Ui_class:
+    def __init__(self):
+        print('Ui_class 입니다.')
+
+        Kiwoom()
